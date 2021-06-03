8949de48907c50601003537a5f0770fdac4dc889
faseeh1999 <52238275+faseeh1999@users.noreply.github.com>
2021-06-03 18:11:48 +0500

    Initial commit

---
+++ android/app/src/main/java/com/example/power_hub/MainActivity.java
@@ -0,0 +1,6 @@
+package com.example.power_hub;
+
+import io.flutter.embedding.android.FlutterActivity;
+
+public class MainActivity extends FlutterActivity {
+}
---
+++ lib/main.dart
@@ -0,0 +1,21 @@
+import 'package:flutter/material.dart';
+import 'package:power_hub/splash.dart';
+
+void main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+        debugShowCheckedModeBanner: false,
+        title: 'Power Hub',
+        theme: ThemeData(
+          primarySwatch: Colors.amber,
+          visualDensity: VisualDensity.adaptivePlatformDensity,
+        ),
+        home: Splash());
+  }
+}
